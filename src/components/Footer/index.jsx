fc472ab8b4a81f0505f83e0983021d5c8125cfcb
Mario Campbell <mcampbell327@yahoo.com.ar>
2021-07-18 21:01:41 -0300

    modificacion en copyright

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -9,7 +9,7 @@ const Footer = () => {
                 <img className='w-12 ' src={ isoMarioCampbell } alt="Isotipo Mario Campbell"/>
                 <Redes />
                 <p className='flex flex-col md:flex-row text-white text-center font-light'>
-                    <span className='md:pr-2'>Todos los derechos reservados 2020.</span>
+                    <span className='md:pr-2'>Todos los derechos reservados 2021.</span>
                     <span>Creado con<i className="hover:text-primary-500 transition-colors duration-300 ease-in-out far fa-heart text-sm px-2"></i>por <a className='hover:text-primary-500 transition-colors duration-300 ease-in-out' href="http://mariocampbell.com.ar" target='_blank' rel="noopener noreferrer"><strong>mcmpbll783</strong></a></span>
                 </p>
         </footer>
